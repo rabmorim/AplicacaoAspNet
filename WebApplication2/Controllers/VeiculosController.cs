cf7ea801ff6c89f3238b9d013ae0c2cc15cf7183
Rodrigo Abreu Amorim <Ramorim13@yahoo.com.br>
2024-11-08 11:57:44 -0300

    Update funcionalidades

--- WebApplication2/Controllers/VeiculosController.cs
+++ WebApplication2/Controllers/VeiculosController.cs
@@ -75,6 +75,49 @@ namespace WebApplication2.Controllers
 
             return View(veiculo);
         }
+        //Método Get Details do veiculo
+        public async Task<IActionResult> Details (int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var dados = await _context.Veiculos.FindAsync(id);
+
+            if (dados == null)
+                return NotFound();
+
+            return View(dados);
+        }
+
+        // Método Get Delete
+        public async Task<IActionResult> Delete (int? id) 
+        {
+            if (id == null)
+                return NotFound();
+
+            var dados = await _context.Veiculos.FindAsync(id);
+
+            if (dados == null)
+                return NotFound();
+
+            return View(dados);
+        }
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var dados = await _context.Veiculos.FindAsync(id);
+
+            if (dados == null)
+                return NotFound();
+
+            _context.Veiculos.Remove(dados);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
     }
 }
 
